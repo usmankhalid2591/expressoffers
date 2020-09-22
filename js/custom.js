f7238ef4c8a97d80a62224d0eecd3ea4cd407231
Usman Khalid <usmankhalid2591@users.noreply.github.com>
2020-09-22 18:25:36 +0500

    Changes.

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,25 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots: true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
+$(document).ready(function () {
+
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
         }
+    });
+
+    if ($(":button").hasClass("collapsed")) {
+        $(':button').addClass('faq-chevron-up');
     }
-})
+}); 
